1b3fa0af7fcd2a547de17655f9535dbf75ab0dba
zhaosong <zhaosong_204@163.com>
2018-03-14 18:19:22 +0800

    MOD: eslint

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { AppRegistry } from 'react-native'
-import { AppContainer } from 'fego-rn'
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { AppContainer } from 'fego-rn';
 
-const App = props => {
-	// 更新AppInfo后，再加载src下的模块
-	const AppInfo = require('./js/src/AppInfo').default;
-	AppInfo.init(props);
-	const AppSetContainer = AppContainer.setApp(require('./js/src').default);
-	// 确保AppInfo的信息正确
-	return <AppSetContainer />
-}
+const App = (props) => {
+  // 更新AppInfo后，再加载src下的模块
+  const AppInfo = require('./js/src/AppInfo').default;
+  AppInfo.init(props);
+  const AppSetContainer = AppContainer.setApp(require('./js/src').default);
+  // 确保AppInfo的信息正确
+  return <AppSetContainer />;
+};
 AppRegistry.registerComponent('fego', () => App);
--- js/src/main/Route.js
+++ js/src/main/Route.js
@@ -6,27 +6,6 @@ import { View } from 'react-native';
 import { Icon } from 'fego-rn';
 // import default from '../common/Style';
 
-// function GetTabIcon() {
-//   const { name, iconAttr } = this.props;
-//   // tabBarIcon的父标签必须设置高度以避免icon与label重叠，bug详见： https://github.com/react-community/react-navigation/issues/2805
-//   return (
-//     <View style={{ height: 26 }}>
-//       <Icon name={name} family="nsip" style={{ color: iconAttr.tintColor, fontSize: 22 }} />
-//     </View>);
-// }
-// const TabIcon = GetTabIcon();
-
-// /* eslint react/prefer-stateless-function: "off" */
-// class TabIcon extends React.Component {
-//   render() {
-//     const { name, iconAttr } = this.props;
-//     // tabBarIcon的父标签必须设置高度以避免icon与label重叠，bug详见： https://github.com/react-community/react-navigation/issues/2805
-//     return (
-//       <View style={{ height: 26 }}>
-//         <Icon name={name} family="nsip" style={{ color: iconAttr.tintColor, fontSize: 22 }} />
-//       </View>);
-//   }
-// }
 const TabIcon = (props) => {
   const { name, iconAttr } = props;
   // tabBarIcon的父标签必须设置高度以避免icon与label重叠，bug详见： https://github.com/react-community/react-navigation/issues/2805
