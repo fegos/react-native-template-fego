8dcd2d0685ccaa490701ed1d9619f9b06b2f2d57
zhaosong <zhaosong_204@163.com>
2018-03-14 18:22:50 +0800

    DEL: 移除ip.txt

--- init.sh
+++ init.sh
@@ -29,7 +29,7 @@ echo "+++++++++mod AppDelegate+++++++++"
 cd ..
 chmod u+x ./tools/sed.sh
 ./tools/sed.sh
-ruby tools/xcodeFileManager.rb ../ios $projectname add ip.txt
+# ruby tools/xcodeFileManager.rb ../ios $projectname add ip.txt
 ruby tools/xcodeFileManager.rb ../ios $projectname del Libraries
 ruby tools/xcodeFileManager.rb ../ios $projectname del $projectname/main.jsbundle
 ruby tools/xcodeFileManager.rb ../ios $projectname script SetIP  "ip=\$(ifconfig | grep 'inet 10.' | head -1 | cut -d \" \" -f 2)+--+if [ ! -f ip.txt ]; then+--+touch ip.txt+--+ruby ../tools/xcodeFileManager.rb ../ios $projectname add ip.txt+--+fi+--+echo \$ip >ip.txt" 0
